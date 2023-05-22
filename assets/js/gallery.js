57fa9129da489e4a3b1fb61fa67c7428930b3d86
MarlaRey <hejmarlenehej@gmail.com>
2023-05-22 12:52:05 +0200

    der loades nu billeder på siden
    stadig ikke færdig

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -7,38 +7,51 @@ window.addEventListener("load", generateGrid);
 
 // globale variabler
 
-const myImages = []; // til array opgaven, byg dit array af billed urlér her.
+const myImages = ['assets/img/Brachypelma_smithi.jpg', 'assets/img/elephant.jpg', 'assets/img/great-white.jpg', 'assets/img/koala.jpg', 'assets/img/standard_tiger.jpg']; // til array opgaven, byg dit array af billed urlér her.
 const myApp = document.getElementById("app");
 
 //Kalder funktionen Clear app 
 //Loop gennem array'et myImages og send url til buildCard 
-function generateGrid(){
+function generateGrid() {
+    clearApp();
 
+    myImages.map((imageUrl) => {
+        buildCard(imageUrl);
+    });
 }
 
 //Kræver en url 
 //Opretter article tag 
 //I article tag skal der oprettes et img tag med src fra tilsendt url 
 //Add article til app dom element 
-//Tilføj eventlistener der kan det som beskrevet i clickSmall 
-function buildCard(myUrl){
+//Tilføj eventlistener tilknyttet img'en, der kan det som beskrevet i clickSmall 
+function buildCard(myUrl) {
+    let myArticleTag = document.createElement('article');
+    
+    let myImageTag = document.createElement('img');
+    myImageTag.src = myUrl;
+    myImageTag.addEventListener('click', clickSmall);
 
+    myArticleTag.appendChild(myImageTag);
+
+    myApp.appendChild(myArticleTag);
 }
+
 //Kræver et event eller skal være eventlistener 
 //finder den url i myImages der er klikket på og send url til BuildBig 
-function clickSmall(event){
+function clickSmall(event) {
 
 }
 
 //Kald clearApp 
 //Byg article element med img elemenet med scr fra url 
 //Tilføj eventlistener til billede med click der kalder generateGrid 
-function BuildBig(myUrl){
+function BuildBig(myUrl) {
 
 }
 
 //slet alt html i app element 
-function clearApp(){
-
+function clearApp() {
+    myApp.innerHTML = ""
 }
 
